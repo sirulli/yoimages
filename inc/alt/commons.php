a0bcfa5091b3d9913ea4111798ce8a2cc48c49c4
matteo.cajani <matteo.cajani@gmail.com>
2014-10-09 01:57:20 +0200

    done set alt value compared to other existing post parent attachments (todo: same for title)

--- inc/alt/commons.php
+++ inc/alt/commons.php
@@ -5,13 +5,34 @@ if ( ! defined ( 'ABSPATH' ) ) {
 }
 
 function wprie_alt_get_image_seo_title( $attachment, $parent ) {
+	$base_title = $parent->post_title;
+	$title = $base_title;
+	$count = 1;
 	// TODO set value compared to other existing post parent attachments
-	$title = $parent->post_title;
 	return $title;
 }
 
 function wprie_alt_get_image_seo_alt( $attachment, $parent ) {
-	// TODO set value compared to other existing post parent attachments
-	$alt = $parent->post_title;
+	$base_alt = $parent->post_title;
+	$alt = $base_alt;
+	$count = 1;
+	$args = array(
+		'post_type' => 'attachment',
+		'post_status' => 'any',
+		'posts_per_page' => 1,
+		'meta_query' => array(
+			array(
+				'key' => '_wp_attachment_image_alt',
+				'value' => $alt
+			)
+		)
+	);
+	$query = new WP_Query( $args );
+	while ( $query->post_count > 0 ) {
+		$alt = $base_alt . ' ' . $count;
+		$args['meta_query'][0]['value'] = $alt;
+		$query = new WP_Query( $args );
+		$count++;
+	}	
 	return $alt;
 }
